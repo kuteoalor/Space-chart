afc28e23c49dcf585d54c7ca79c027776a08b1b4
Mikhail Sirotyuk <donald.spongeputin@gmail.com>
2023-08-22 21:49:29 +0300

    Some buttons & slider

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,15 +31,23 @@
             diagram = new ScottPlot.FormsPlot();
             ReadFromFileButton = new Button();
             openFileDialog1 = new OpenFileDialog();
+            SunButton = new Button();
+            starScroll = new TrackBar();
+            label1 = new Label();
+            starBox = new TextBox();
+            clearButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)starScroll).BeginInit();
             SuspendLayout();
             // 
             // diagram
             // 
+            diagram.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             diagram.AutoSize = true;
-            diagram.Location = new Point(489, 13);
+            diagram.Location = new Point(771, 13);
             diagram.Margin = new Padding(5, 4, 5, 4);
+            diagram.MaximumSize = new Size(1080, 1080);
             diagram.Name = "diagram";
-            diagram.Size = new Size(939, 780);
+            diagram.Size = new Size(657, 749);
             diagram.TabIndex = 0;
             diagram.Load += diagram_Load;
             // 
@@ -57,15 +65,75 @@
             // 
             openFileDialog1.FileName = "openFileDialog1";
             // 
+            // SunButton
+            // 
+            SunButton.Enabled = false;
+            SunButton.Location = new Point(12, 117);
+            SunButton.Name = "SunButton";
+            SunButton.Size = new Size(390, 53);
+            SunButton.TabIndex = 2;
+            SunButton.Text = "Подсветить Солнце";
+            SunButton.UseVisualStyleBackColor = true;
+            SunButton.Visible = false;
+            SunButton.Click += SunButton_Click;
+            // 
+            // starScroll
+            // 
+            starScroll.LargeChange = 10000;
+            starScroll.Location = new Point(12, 248);
+            starScroll.Maximum = 200000;
+            starScroll.Name = "starScroll";
+            starScroll.Size = new Size(390, 56);
+            starScroll.SmallChange = 5000;
+            starScroll.TabIndex = 3;
+            starScroll.Value = 1000;
+            starScroll.Scroll += starScroll_Scroll;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(12, 198);
+            label1.Name = "label1";
+            label1.Size = new Size(258, 31);
+            label1.TabIndex = 4;
+            label1.Text = "Сколько звезд считать?";
+            // 
+            // starBox
+            // 
+            starBox.Location = new Point(277, 202);
+            starBox.Name = "starBox";
+            starBox.Size = new Size(125, 27);
+            starBox.TabIndex = 5;
+            starBox.Text = "1000";
+            starBox.TextChanged += starBox_TextChanged;
+            // 
+            // clearButton
+            // 
+            clearButton.Location = new Point(12, 310);
+            clearButton.Name = "clearButton";
+            clearButton.Size = new Size(390, 53);
+            clearButton.TabIndex = 6;
+            clearButton.Text = "Очистить диаграмму";
+            clearButton.UseVisualStyleBackColor = true;
+            clearButton.Click += clearButton_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1442, 775);
+            Controls.Add(clearButton);
+            Controls.Add(starBox);
+            Controls.Add(label1);
+            Controls.Add(starScroll);
+            Controls.Add(SunButton);
             Controls.Add(ReadFromFileButton);
             Controls.Add(diagram);
+            MinimumSize = new Size(1200, 700);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)starScroll).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -75,5 +143,10 @@
         private ScottPlot.FormsPlot diagram;
         private Button ReadFromFileButton;
         private OpenFileDialog openFileDialog1;
+        private Button SunButton;
+        private TrackBar starScroll;
+        private Label label1;
+        private TextBox starBox;
+        private Button clearButton;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -7,11 +7,11 @@ namespace SpaceChart
     public partial class Form1 : Form
     {
         private string[] lines;
-        private List<Star> StarList;
+        uint AmountOfStars = 1000;
+        //private List<Star> StarList;
         public Form1()
         {
             InitializeComponent();
-
         }
 
         private void diagram_Load(object sender, EventArgs e)
@@ -19,23 +19,11 @@ namespace SpaceChart
             diagram.Plot.XAxis.TickLabelNotation(invertSign: true);
             diagram.Plot.YAxis.TickLabelNotation(invertSign: true);
             diagram.Plot.Style(ScottPlot.Style.Blue2);
+            diagram.Plot.YLabel("Абсолютная звездная величина");
+            diagram.Plot.XLabel("Температура, К");
+            diagram.Plot.Title("Диаграмма Герцшпрунга-Рассела", size: 25);
+            diagram.Plot.Render(lowQuality:true);
             diagram.Refresh();
-            //var cmap = ScottPlot.Drawing.Colormap.Turbo;
-            //diagram.Plot.AddColorbar(cmap);
-
-            //Random rand = new(0);
-
-            //for (int i = 0; i < 1000; i++)
-            //{
-            //    double x = ScottPlot.DataGen.RandomNormalValue(rand, mean: 0.1, stdDev: .7);
-            //    double y = ScottPlot.DataGen.RandomNormalValue(rand, mean: 0.1, stdDev: .7);
-            //    double colorFranction = Math.Sqrt(x * x + y * y);
-            //    System.Drawing.Color c = ScottPlot.Drawing.Colormap.Turbo.GetColor(colorFranction);
-            //    diagram.Plot.AddPoint(x, y, c);
-            //}
-
-            //diagram.Plot.AddPoint(3, 3, ScottPlot.Drawing.Colormap.Turbo.GetColor(Math.Sqrt(3 * 3 + 3 * 3)));
-            //diagram.Refresh();
         }
 
         private void ReadFromFileButton_Click(object sender, EventArgs e)
@@ -45,14 +33,48 @@ namespace SpaceChart
             var path = Path.Combine(Environment.CurrentDirectory, filename);
             lines = File.ReadAllLines(path);
             diagram.Plot.Clear();
-            //diagram.Plot.XAxis.TickLabelNotation(invertSign: true);
-            //diagram.Plot.YAxis.TickLabelNotation(invertSign: true);
-            //foreach ( var line in lines )
-            for (int i = 0; i < 200000; i++)
+            for (int i = 0; i < AmountOfStars; i++)
             {
                 var star = new Star(lines[i]);
                 diagram.Plot.AddPoint(-star.Temperature, -star.AbsoluteMagnitude, star.color, 2);
             }
+            diagram.Refresh();
+            SunButton.Enabled = true;
+            SunButton.Visible = true;
+        }
+
+        private void SunButton_Click(object sender, EventArgs e)
+        {
+            //diagram.Plot.AddMarker(-5778, -4.83, MarkerShape.filledDiamond, 10, Color.Yellow);
+            diagram.Plot.AddPoint(-5778, -4.83, Color.Red, 7);
+            diagram.Refresh();
+        }
+
+        private void starScroll_Scroll(object sender, EventArgs e)
+        {
+            starBox.Text = starScroll.Value.ToString();
+            //AmountOfStars = (uint)starScroll.Value;
+        }
+
+        private void starBox_TextChanged(object sender, EventArgs e)
+        {
+            if (!uint.TryParse(starBox.Text, out AmountOfStars) && !string.IsNullOrEmpty(starBox.Text))
+            {
+                MessageBox.Show("Введите число в диапазоне от 1000 до 200000!");
+            }
+            else
+            {
+                if (AmountOfStars > 200000)
+                {
+                    MessageBox.Show("Введите число в диапазоне от 1000 до 200000!");
+                    AmountOfStars = 200000;
+                }
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            diagram.Plot.Clear();
             diagram.Refresh();
         }
     }
