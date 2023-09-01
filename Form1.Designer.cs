7b110e607d7a233ee6526473ba2e1a2820b91d41
Mikhail Sirotyuk <donald.spongeputin@gmail.com>
2023-09-01 21:01:05 +0300

    Luminosity classes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,6 +36,15 @@
             starBox = new TextBox();
             clearButton = new Button();
             SunCheckBox = new CheckBox();
+            MainSequenceBox = new CheckBox();
+            button1 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            button2 = new Button();
+            WhiteDwarfsBox = new CheckBox();
+            redGiantsBox = new CheckBox();
+            BrightGiantsBox = new CheckBox();
+            SuperGiantsBox = new CheckBox();
             ((System.ComponentModel.ISupportInitialize)starScroll).BeginInit();
             SuspendLayout();
             // 
@@ -43,19 +52,19 @@
             // 
             diagram.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             diagram.AutoSize = true;
-            diagram.Location = new Point(771, 13);
+            diagram.Location = new Point(458, 13);
             diagram.Margin = new Padding(5, 4, 5, 4);
             diagram.MaximumSize = new Size(1080, 1080);
             diagram.Name = "diagram";
-            diagram.Size = new Size(657, 749);
+            diagram.Size = new Size(970, 749);
             diagram.TabIndex = 0;
             diagram.Load += diagram_Load;
             // 
             // ReadFromFileButton
             // 
-            ReadFromFileButton.Location = new Point(12, 45);
+            ReadFromFileButton.Location = new Point(12, 125);
             ReadFromFileButton.Name = "ReadFromFileButton";
-            ReadFromFileButton.Size = new Size(390, 53);
+            ReadFromFileButton.Size = new Size(407, 53);
             ReadFromFileButton.TabIndex = 1;
             ReadFromFileButton.Text = "Прочесть данные из файла";
             ReadFromFileButton.UseVisualStyleBackColor = true;
@@ -68,10 +77,10 @@
             // starScroll
             // 
             starScroll.LargeChange = 10000;
-            starScroll.Location = new Point(12, 248);
+            starScroll.Location = new Point(12, 70);
             starScroll.Maximum = 200000;
             starScroll.Name = "starScroll";
-            starScroll.Size = new Size(390, 56);
+            starScroll.Size = new Size(407, 56);
             starScroll.SmallChange = 5000;
             starScroll.TabIndex = 3;
             starScroll.Value = 1000;
@@ -81,7 +90,7 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(12, 198);
+            label1.Location = new Point(12, 33);
             label1.Name = "label1";
             label1.Size = new Size(258, 31);
             label1.TabIndex = 4;
@@ -89,7 +98,7 @@
             // 
             // starBox
             // 
-            starBox.Location = new Point(277, 202);
+            starBox.Location = new Point(276, 37);
             starBox.Name = "starBox";
             starBox.Size = new Size(125, 27);
             starBox.TabIndex = 5;
@@ -98,19 +107,20 @@
             // 
             // clearButton
             // 
-            clearButton.Location = new Point(12, 310);
+            clearButton.Location = new Point(12, 289);
             clearButton.Name = "clearButton";
-            clearButton.Size = new Size(390, 53);
+            clearButton.Size = new Size(407, 53);
             clearButton.TabIndex = 6;
             clearButton.Text = "Очистить диаграмму";
             clearButton.UseVisualStyleBackColor = true;
+            clearButton.Visible = false;
             clearButton.Click += clearButton_Click;
             // 
             // SunCheckBox
             // 
             SunCheckBox.AutoSize = true;
             SunCheckBox.CheckAlign = ContentAlignment.MiddleRight;
-            SunCheckBox.Location = new Point(12, 131);
+            SunCheckBox.Location = new Point(12, 249);
             SunCheckBox.Name = "SunCheckBox";
             SunCheckBox.Size = new Size(167, 24);
             SunCheckBox.TabIndex = 7;
@@ -119,11 +129,123 @@
             SunCheckBox.Visible = false;
             SunCheckBox.CheckedChanged += SunCheckBox_CheckedChanged;
             // 
+            // MainSequenceBox
+            // 
+            MainSequenceBox.AutoSize = true;
+            MainSequenceBox.CheckAlign = ContentAlignment.MiddleRight;
+            MainSequenceBox.Location = new Point(12, 189);
+            MainSequenceBox.Name = "MainSequenceBox";
+            MainSequenceBox.Size = new Size(234, 24);
+            MainSequenceBox.TabIndex = 8;
+            MainSequenceBox.Text = "Главная последовательность";
+            MainSequenceBox.UseVisualStyleBackColor = true;
+            MainSequenceBox.Visible = false;
+            MainSequenceBox.CheckedChanged += MainSequenceBox_CheckedChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(121, 539);
+            button1.Name = "button1";
+            button1.Size = new Size(221, 29);
+            button1.TabIndex = 9;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(62, 603);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 20);
+            label2.TabIndex = 10;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(168, 605);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 20);
+            label3.TabIndex = 11;
+            label3.Text = "label3";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(93, 665);
+            button2.Name = "button2";
+            button2.Size = new Size(249, 36);
+            button2.TabIndex = 12;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // WhiteDwarfsBox
+            // 
+            WhiteDwarfsBox.AutoSize = true;
+            WhiteDwarfsBox.CheckAlign = ContentAlignment.MiddleRight;
+            WhiteDwarfsBox.Location = new Point(263, 189);
+            WhiteDwarfsBox.Name = "WhiteDwarfsBox";
+            WhiteDwarfsBox.Size = new Size(138, 24);
+            WhiteDwarfsBox.TabIndex = 13;
+            WhiteDwarfsBox.Text = "Белые Карлики";
+            WhiteDwarfsBox.UseVisualStyleBackColor = true;
+            WhiteDwarfsBox.Visible = false;
+            WhiteDwarfsBox.CheckedChanged += WhiteDwarfsBox_CheckedChanged;
+            // 
+            // redGiantsBox
+            // 
+            redGiantsBox.AutoSize = true;
+            redGiantsBox.CheckAlign = ContentAlignment.MiddleRight;
+            redGiantsBox.Location = new Point(12, 219);
+            redGiantsBox.Name = "redGiantsBox";
+            redGiantsBox.Size = new Size(152, 24);
+            redGiantsBox.TabIndex = 14;
+            redGiantsBox.Text = "Красные Гиганты";
+            redGiantsBox.UseVisualStyleBackColor = true;
+            redGiantsBox.Visible = false;
+            redGiantsBox.CheckedChanged += redGiantsBox_CheckedChanged;
+            // 
+            // BrightGiantsBox
+            // 
+            BrightGiantsBox.AutoSize = true;
+            BrightGiantsBox.CheckAlign = ContentAlignment.MiddleRight;
+            BrightGiantsBox.Location = new Point(168, 219);
+            BrightGiantsBox.Name = "BrightGiantsBox";
+            BrightGiantsBox.Size = new Size(133, 24);
+            BrightGiantsBox.TabIndex = 15;
+            BrightGiantsBox.Text = "Яркие Гиганты";
+            BrightGiantsBox.UseVisualStyleBackColor = true;
+            BrightGiantsBox.Visible = false;
+            BrightGiantsBox.CheckedChanged += BrightGiants_CheckedChanged;
+            // 
+            // SuperGiantsBox
+            // 
+            SuperGiantsBox.AutoSize = true;
+            SuperGiantsBox.CheckAlign = ContentAlignment.MiddleRight;
+            SuperGiantsBox.Location = new Point(307, 219);
+            SuperGiantsBox.Name = "SuperGiantsBox";
+            SuperGiantsBox.Size = new Size(127, 24);
+            SuperGiantsBox.TabIndex = 16;
+            SuperGiantsBox.Text = "Сверхгиганты";
+            SuperGiantsBox.UseVisualStyleBackColor = true;
+            SuperGiantsBox.Visible = false;
+            SuperGiantsBox.CheckedChanged += SuperGiantsBox_CheckedChanged;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1442, 775);
+            Controls.Add(SuperGiantsBox);
+            Controls.Add(BrightGiantsBox);
+            Controls.Add(redGiantsBox);
+            Controls.Add(WhiteDwarfsBox);
+            Controls.Add(button2);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(MainSequenceBox);
             Controls.Add(SunCheckBox);
             Controls.Add(clearButton);
             Controls.Add(starBox);
@@ -149,5 +271,14 @@
         private TextBox starBox;
         private Button clearButton;
         private CheckBox SunCheckBox;
+        private CheckBox MainSequenceBox;
+        private Button button1;
+        private Label label2;
+        private Label label3;
+        private Button button2;
+        private CheckBox WhiteDwarfsBox;
+        private CheckBox redGiantsBox;
+        private CheckBox BrightGiantsBox;
+        private CheckBox SuperGiantsBox;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,8 @@
 using ScottPlot;
+using ScottPlot.Plottable;
 using System.IO;
 using System.Linq.Expressions;
+using System.Security;
 using System.Security.Cryptography.X509Certificates;
 
 namespace SpaceChart
@@ -9,6 +11,7 @@ namespace SpaceChart
     {
         private string[] lines;
         uint AmountOfStars = 1000;
+        DraggableMarkerPlot myDraggableMarker = new ScottPlot.Plottable.DraggableMarkerPlot();
         //private List<Star> StarList;
         public Form1()
         {
@@ -38,8 +41,15 @@ namespace SpaceChart
                 var star = new Star(lines[i]);
                 diagram.Plot.AddPoint(-star.Temperature, -star.AbsoluteMagnitude, star.color, 2);
             }
+            diagram.Plot.Legend();
             diagram.Refresh();
             SunCheckBox.Visible = true;
+            MainSequenceBox.Visible = true;
+            clearButton.Visible = true;
+            BrightGiantsBox.Visible = true;
+            redGiantsBox.Visible = true;
+            SuperGiantsBox.Visible = true;
+            WhiteDwarfsBox.Visible = true;
         }
 
 
@@ -71,13 +81,146 @@ namespace SpaceChart
             diagram.Plot.Clear();
             diagram.Refresh();
             SunCheckBox.Visible = false;
+            SunCheckBox.Checked = false;
+            MainSequenceBox.Visible = false;
+            MainSequenceBox.Checked = false;
+            clearButton.Visible = false;
+            WhiteDwarfsBox.Visible = false;
+            redGiantsBox.Visible = false;
+            SuperGiantsBox.Visible = false;
+            BrightGiantsBox.Visible = false;
         }
 
         private void SunCheckBox_CheckedChanged(object sender, EventArgs e)
         {
             if (SunCheckBox.Checked)
             {
-                diagram.Plot.AddPoint(-5778, -4.83, Color.Red, 7);
+                diagram.Plot.AddPoint(-5778, -4.83, Color.Red, 7, label: "Солнце");
+                diagram.Refresh();
+            }
+            else
+            {
+                diagram.Plot.Clear();
+                for (int i = 0; i < AmountOfStars; i++)
+                {
+                    var star = new Star(lines[i]);
+                    diagram.Plot.AddPoint(-star.Temperature, -star.AbsoluteMagnitude, star.color, 2);
+                }
+                diagram.Refresh();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myDraggableMarker.X = -3800;
+            myDraggableMarker.Y = -9;
+            myDraggableMarker.Color = Color.Magenta;
+            myDraggableMarker.MarkerShape = MarkerShape.filledCircle;
+            myDraggableMarker.MarkerSize = 5;
+            diagram.Plot.Add(myDraggableMarker);
+            diagram.Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = myDraggableMarker.X.ToString();
+            label3.Text = myDraggableMarker.Y.ToString();
+        }
+
+        private void MainSequenceBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (MainSequenceBox.Checked)
+            {
+                double[] xs = {-3857, -3930, -4056, -4197, -4358, -4598, -4798, -5020, -5318, -5620,
+                    -5788, -6018, -6227, -6453, -6776, -7153, -7562, -8159, -8615, -9453, -10510};
+                double[] ys = {-9, -8.5, -8, -7.6, -7.375, -6.937, -6.518, -6.118, -5.654, -5.176, -4.929,
+                -4.5, -4.122, -3.483, -2.976, -2.437, -1.904, -1.464, -0.974, -0.533, -0.215};
+                diagram.Plot.AddScatter(xs, ys, Color.Yellow, 3, 1, label: "Главная последовательность");
+                diagram.Refresh();
+            }
+            else
+            {
+                diagram.Plot.Clear();
+                for (int i = 0; i < AmountOfStars; i++)
+                {
+                    var star = new Star(lines[i]);
+                    diagram.Plot.AddPoint(-star.Temperature, -star.AbsoluteMagnitude, star.color, 2);
+                }
+                diagram.Refresh();
+            }
+        }
+
+        private void WhiteDwarfsBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (WhiteDwarfsBox.Checked)
+            {
+                double[] xs = { -4576, -6084, -7434, -9363, -11485 };
+                double[] ys = { -15.645, -14.378, -13.563, -11.889, -10.938 };
+                diagram.Plot.AddScatter(xs, ys, Color.White, 3, 1, label: "Белые Карлики");
+                diagram.Refresh();
+            }
+            else
+            {
+                diagram.Plot.Clear();
+                for (int i = 0; i < AmountOfStars; i++)
+                {
+                    var star = new Star(lines[i]);
+                    diagram.Plot.AddPoint(-star.Temperature, -star.AbsoluteMagnitude, star.color, 2);
+                }
+                diagram.Refresh();
+            }
+
+        }
+
+        private void redGiantsBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (redGiantsBox.Checked)
+            {
+                double[] xs = { -5043, -4623, -4288, -4105, -3755, -3633 };
+                double[] ys = { -2.942, -1.989, -0.918, 0.321, 1.391, 2.602 };
+                diagram.Plot.AddScatter(xs, ys, Color.Red, 3, 1, label: "Красные Гиганты");
+                diagram.Refresh();
+            }
+            else
+            {
+                diagram.Plot.Clear();
+                for (int i = 0; i < AmountOfStars; i++)
+                {
+                    var star = new Star(lines[i]);
+                    diagram.Plot.AddPoint(-star.Temperature, -star.AbsoluteMagnitude, star.color, 2);
+                }
+                diagram.Refresh();
+            }
+        }
+
+        private void BrightGiants_CheckedChanged(object sender, EventArgs e)
+        {
+            if (BrightGiantsBox.Checked)
+            {
+                double[] ys = { 6.755, 6.336, 6.4566, 6.336, 6.277, 6.576, 7.174 };
+                double[] xs = { -3603, -4536, -5632, -6939, -8362, -11022, -12048 };
+                diagram.Plot.AddScatter(xs, ys, Color.DeepSkyBlue, 3, 1, label: "Яркие Гиганты");
+                diagram.Refresh();
+            }
+            else
+            {
+                diagram.Plot.Clear();
+                for (int i = 0; i < AmountOfStars; i++)
+                {
+                    var star = new Star(lines[i]);
+                    diagram.Plot.AddPoint(-star.Temperature, -star.AbsoluteMagnitude, star.color, 2);
+                }
+                diagram.Refresh();
+            }
+        }
+
+        private void SuperGiantsBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (SuperGiantsBox.Checked)
+            {
+                double[] xs = { -3556, -4449, -5302, -6209, -7276, -8410, -9451, -10878 };
+                double[] ys = { 11.843, 10.918, 10.610, 10.507, 10.37, 10.644, 10.884, 11.157 };
+                diagram.Plot.AddScatter(xs, ys, Color.Purple, 3, 1, label: "Сверхгиганты");
                 diagram.Refresh();
             }
             else
